3bdadd29a3f68a785a7e2d35157a04d8156cc65f
Matt Rivas <mrivas@mrivas.dev>
2025-02-18 17:56:18 -0300

    Fixed nextJS issue with window object

--- src/app/mac/page.tsx
+++ src/app/mac/page.tsx
@@ -1,11 +1,14 @@
-"use client";
-
-import TerminalMac from "@/components/terminal/macos";
+"use client"
+import dynamic from "next/dynamic";
 import Link from "next/link";
 
-export default function Landing() {
-  return (
 
+const TerminalMac = dynamic(() => import("@/components/terminal/macos"), {
+  ssr: false // This ensures the component is not SSR'd
+});
+export default function MacLanding() {
+
+  return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
         style={{
           backgroundImage: `url('/macosbg.png')`,
